1343ea2baf0c3f8aee1546fe060ce62749f0b268
twregis05 <twregis05@gmail.com>
2025-08-06 01:17:28 -0400

    Initial Commit

---
+++ FutFacts/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FutFacts
+//
+//  Created by Trenton Regis on 8/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
